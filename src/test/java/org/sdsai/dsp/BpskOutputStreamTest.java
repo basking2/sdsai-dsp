59ef99835048c3130ad88f12dbdc91332bb669bf
Sam Baskinger <basking2@yahoo.com>
2014-01-24 10:37:28 -0600

    Remove writing test files.

--- src/test/java/org/sdsai/dsp/BpskOutputStreamTest.java
+++ src/test/java/org/sdsai/dsp/BpskOutputStreamTest.java
@@ -14,30 +14,45 @@ public class BpskOutputStreamTest {
 
     @Test
     public void writeToSpeakers() throws IOException, LineUnavailableException {
-        BpskGenerator psk = new BpskGenerator(700, 11025, 31.25);
-
+        final BpskGenerator         psk  = new BpskGenerator(700, 11025, 31.25);
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        final SourceDataLine        sdl  = AudioSystem.getSourceDataLine(psk.getAudioFormat());
-        final BpskOutputStream      os   = new BpskOutputStream(baos, psk);
 
+        /* Output. */
+        final BpskOutputStream      os   = new BpskOutputStream(baos, psk);
         os.preamble(10);
         os.write("This is a very nice test.".getBytes());
         os.postamble(10);
         os.close();
 
+        /* Audio. */
+        final SourceDataLine sdl  = AudioSystem.getSourceDataLine(psk.getAudioFormat());
         sdl.open(psk.getAudioFormat());
         sdl.start();
-
-        final byte[] b = baos.toByteArray();
-
-        sdl.write(b, 0, b.length);
-
+        sdl.write(baos.toByteArray(), 0, baos.size());
         sdl.drain();
         sdl.stop();
         sdl.close();
+    }
 
-        java.io.FileOutputStream fos = new java.io.FileOutputStream("a.raw");
-        fos.write(b);
-        fos.close();
+    @Test
+    public void writeToSpeakers2x() throws IOException, LineUnavailableException {
+        final BpskGenerator         psk  = new BpskGenerator(700, 11025, 62.50);
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        /* Output. */
+        final BpskOutputStream      os   = new BpskOutputStream(baos, psk);
+        os.preamble(10);
+        os.write("This is a very nice test.".getBytes());
+        os.postamble(10);
+        os.close();
+
+        /* Audio. */
+        final SourceDataLine sdl  = AudioSystem.getSourceDataLine(psk.getAudioFormat());
+        sdl.open(psk.getAudioFormat());
+        sdl.start();
+        sdl.write(baos.toByteArray(), 0, baos.size());
+        sdl.drain();
+        sdl.stop();
+        sdl.close();
     }
 }
